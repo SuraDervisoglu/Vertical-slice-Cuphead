3fe867071005e41cb79991bd8916e7c330b683be
OpiumBird <145978078+MarcosMartinsCarvalho@users.noreply.github.com>
2024-11-27 12:35:23 +0100

    platform v2
    platform moves down when dead

--- Cuphead Vertical slice/Assets/scripts/Platform.cs
+++ Cuphead Vertical slice/Assets/scripts/Platform.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
 
-public class platform : MonoBehaviour
+public class Platform : MonoBehaviour
 {
     [SerializeField] private float speed = 5f; // Speed of movement
     [SerializeField] private float timeA = 1f; // Minimum time to move up
     [SerializeField] private float timeB = 3f; // Maximum time to move up
+    [SerializeField] private float deathSpeed = 2f; // Speed of movement when dying
+    [SerializeField] private float despawnTime = 2f; // Time before the platform despawns
 
     private bool isMoving = true;
 
@@ -18,6 +20,11 @@ public class platform : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Die();
+        }
+
         if (isMoving)
         {
             // Move the object straight up
@@ -29,5 +36,35 @@ public class platform : MonoBehaviour
     {
         yield return new WaitForSeconds(duration);
         isMoving = false; // Stop moving after the duration
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyBullet"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Start moving the platform down
+        StartCoroutine(MoveDownAndDespawn());
+    }
+
+    private IEnumerator MoveDownAndDespawn()
+    {
+        while (true)
+        {
+            // Move the object straight down
+            transform.Translate(Vector2.down * deathSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        // Wait for a short time before despawning
+        yield return new WaitForSeconds(despawnTime);
+
+        // Destroy the platform game object
+        Destroy(gameObject);
     }
 }
